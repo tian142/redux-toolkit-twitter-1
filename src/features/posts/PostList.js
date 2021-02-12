d0ab850d217ed8e9482a989a3dbb9cee733c3229
Tian <tian.qiu142@gmail.com>
2021-02-12 14:22:56 -0800

    completed useSelector and mapping logic to retrieve posts

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+
 export const PostList = () => {
-  return <div></div>
+  const posts = useSelector((state) => state.posts)
+
+  const mapPosts = posts.map((post) => (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </article>
+  ))
+
+  return <section>{mapPosts}</section>
 }
