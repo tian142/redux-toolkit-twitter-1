71115b1829a2198072f05332dab07fa95a572b90
Tian <tian.qiu142@gmail.com>
2021-02-12 14:14:27 -0800

    populated postsslice with initial state, name

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,3 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-createSlice
+const initialState = [
+  {
+    id: 1,
+    title: 'Post 1',
+    content: 'Hello, this is my first Post',
+  },
+  {
+    id: 2,
+    title: 'post 2',
+    content: 'Hello, this is my second post',
+  },
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+})
+
+export default postsSlice.reducer
